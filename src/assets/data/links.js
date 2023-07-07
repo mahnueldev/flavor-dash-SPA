eaedb71a56374eb6fa8bf07277a1d9827c81641e
mahnueldev <emmanuelgbd3@gmail.com>
2023-07-07 14:37:00 +0100

    netlify config

--- src/assets/data/links.js
+++ src/assets/data/links.js
@@ -1,4 +1,7 @@
- export const documentation = "https://flavourdash.netlify.app/introduction.html";
+ export const documentation = "http://localhost:3000/introduction.html";
  export const github = "https://github.com/mahnueldev/flavor-dash";
- export const website = "https://flavourdash.netlify.app/";
+ export const website = "http://localhost:3000/index.html";
 //  export const settings = "/settings";
+// export const documentation = "https://flavourdash.netlify.app/introduction.html";
+//  export const github = "https://github.com/mahnueldev/flavor-dash";
+//  export const website = "https://flavourdash.netlify.app/";
