0d3b30b428299e6c5c0d1de7350c17772cac42d2
mahnueldev <emmanuelgbd3@gmail.com>
2023-07-07 06:37:40 +0100

    url changes

--- src/assets/data/links.js
+++ src/assets/data/links.js
@@ -1,4 +1,4 @@
- export const documentation = "http://localhost:3000/introduction.html";
+ export const documentation = "https://flavourdash.netlify.app/introduction.html";
  export const github = "https://github.com/mahnueldev/flavor-dash";
- export const website = "http://localhost:3000/index.html";
+ export const website = "https://flavourdash.netlify.app/";
 //  export const settings = "/settings";
